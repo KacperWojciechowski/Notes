4504aaa9037981fbecd428b09f487d037756f270
KacperWojciechowski <k.woj.coding@gmail.com>
2025-11-01 18:43:06 +0100

    remove scripts

--- serverSideRenderer/target/debug/deps/serverSideRenderer-5bdb0db69b702dee.d
+++
@@ -1,5 +0,0 @@
-/home/gerwant/Repos/websiteBackend/serverSideRenderer/target/debug/deps/serverSideRenderer-5bdb0db69b702dee.d: src/main.rs
-
-/home/gerwant/Repos/websiteBackend/serverSideRenderer/target/debug/deps/libserverSideRenderer-5bdb0db69b702dee.rmeta: src/main.rs
-
-src/main.rs:
--- serverSideRenderer/target/debug/serverSideRenderer.d
+++
@@ -1,1 +0,0 @@
-/home/gerwant/Repos/websiteBackend/serverSideRenderer/target/debug/serverSideRenderer: /home/gerwant/Repos/websiteBackend/serverSideRenderer/src/main.rs
